c34868a6333a0d02fa2354e941beb434b929ca64
=Minh Pham <vuminhph@gmail.com>
2020-06-19 09:44:25 +0700

    update report

--- queries.sql
+++ queries.sql
@@ -1,6 +1,4 @@
 -- 1. Get the top 10 best sellers and the number of copies that they sold
-
----- Including returns
 select Title, count(ItemID) No_books
 from Books natural join Order_Items
 where ItemID not in (select ItemID from Returns)
@@ -57,7 +55,7 @@ from (select Name, avg(rating) avg_rating
         group by PublisherID) p;
 
 
--- 5. Get Publishers who sold the most copies (including returns)
+-- 5. Get Publishers who sold the most copies (excluding returns)
 select Name
 from Publishers natural join Books natural join Order_Items
 where ItemID not in (select ItemID from Returns)
@@ -150,19 +148,29 @@ select c.*
 from Customers c natural join Orders natural join Order_Items natural join Returns
 where datediff(return_date, order_date) <= 31;
 
--- 13. Return the percentage of orders that get returned within a month of each genre/category
+-- 13. Find the success percentage (number of successful purchases / total titles) of each category
 select category, round((select count(ItemID)
-                from Orders natural join Order_Items natural join Returns natural join Books b2
-                where b2.category = b1.category
-                and datediff(return_date, order_date) <= 31)
-                /
-                (select count(ItemID)
-                from Order_Items natural join Books b3
-                where b3.category = b1.category) * 100, 1) Percentage
+                        from Order_Items natural join Books b2
+                        where ItemID not in (select ItemID from Returns)
+                        and b2.category = b1.category) 
+                        /
+                        (select count(ItemID)
+                        from Order_Items natural join Books b3
+                        where b3.category = b1.category) * 100, 1) success_percentage
 from Books b1
-group by category;
+group by category
+order by success_percentage desc;
 
--- 13. Customers who make the most orders in the most recent year
+-- 14. Find average value (Dollars per number of pages) of each publisher
+
+select Name, (select avg(round(price / pages_num, 2))
+            from Books b2
+            where b2.PublisherID = b1.PublisherID) average_value
+from Publishers natural join Books b1
+group by PublisherID
+order by average_value desc;
+
+-- 15. Customers who make the most orders in the most recent year
 select Customers.*
 from Customers natural join Orders o1
 where year(order_date) = (select max(year(order_date))
@@ -174,7 +182,7 @@ having count(OrderID) >= all (select count(OrderID)
                                                         from Orders)
                             group by CustomerID);
 
--- 14. Get customers who bought the most copies of book
+-- 16. Get customers who bought the most copies of book
 select concat(first_name, ' ', last_name) Name
 from Customers natural join Orders natural join Order_Items
 where ItemID not in (select ItemID from Returns)
@@ -184,7 +192,18 @@ having count(ItemID) >= all (select count(ItemID)
                             where ItemID not in (select ItemID from Returns)
                             group by CustomerID);
 
--- 15. Get the favourite genre/category of each customer
+-- 17. Get the favourite publisher of each customer
+select concat(first_name, ' ', last_name) Name, p.Name "Favourite publisher"
+from Customers c1 natural join Orders natural join Order_Items natural join Books natural join Publishers p
+where PublisherID = (select PublisherID
+                    from Books natural join Order_Items natural join Orders natural join Customers c2
+                    where c1.CustomerID = c2.CustomerID
+                    group by PublisherID
+                    order by count(ItemID) desc
+                    limit 1)
+-- group by CustomerID;
+
+-- 18. Get the favourite genre/category of each customer
 select concat(first_name, ' ', last_name) Name, category "Favourite category"
 from Customers c1 natural join Orders natural join Order_Items natural join Books
 where category = (select category
@@ -195,7 +214,7 @@ where category = (select category
                 limit 1)
 group by CustomerID;
 
--- 16. Get info of customers who bought more than 2 copies of any book
+-- 19. Get info of customers who bought more than 2 copies of any book
 select c1.*
 from Customers c1 natural join Orders natural join Order_Items b1
 where (select count(distinct ItemID)
@@ -205,7 +224,7 @@ where (select count(distinct ItemID)
         and ItemID not in (select ItemID from Returns)) > 2
 group by CustomerID;
 
--- 17. Return the customers with valid credit card numbers according to Luhn’s algorithm:
+-- 20. Return the customers with valid credit card numbers according to Luhn’s algorithm:
 -- 1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
 -- 2. Add the sum to the sum of the digits that weren’t multiplied by 2.
 -- 3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
@@ -242,3 +261,44 @@ select concat(first_name, ' ', last_name) Customer, Credit_card_no, check_validi
 from Customers
 order by validity desc//
 
+-- BONUS
+
+-- 21. Find the publishers who got the most copies returned
+select Name
+from Publishers natural join Books natural join Order_Items natural join Returns
+group by PublisherID
+having count(ItemID) >= all (select count(ItemID)
+                            from Publishers natural join Books natural join Order_Items natural join Returns
+                            group by PublisherID);
+
+-- 22. Return the percentage of orders that get returned within a month of each genre/category
+select category, round((select count(ItemID)
+                from Orders natural join Order_Items natural join Returns natural join Books b2
+                where b2.category = b1.category
+                and datediff(return_date, order_date) <= 31)
+                /
+                (select count(ItemID)
+                from Order_Items natural join Books b3
+                where b3.category = b1.category) * 100, 1) Percentage
+from Books b1
+group by category;
+
+-- 23. Find the ID and price of the most expensive order
+select OrderID, sum(Price) Gia_Tien
+from Order_Items natural join Books
+group by OrderID
+having Gia_Tien >= all (select sum(Price)
+                        from Order_Items natural join Books
+                        group by OrderID);
+
+-- 24. Find the average rating of each year
+select Published_year, round(avg(rating), 1) 'Average rating'
+from Books
+group by Published_year ;
+
+-- 25. Find the publishers whose all books are rated 3.0 or above
+select Name
+from Publishers
+where PublisherID not in (select PublisherID
+                        from Books
+                        where rating < 3.0);
